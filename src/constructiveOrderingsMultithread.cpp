f15c28e49492070c3794c51422e5cd4c2bb236a7
Zack <zack.baker@lab.kingsu.ca>
2018-10-23 01:22:09 -0600

    Full project rewrite; much more human-readable and a bit faster too

--- src/constructiveOrderingsMultithread.cpp
+++ src/constructiveOrderingsMultithread.cpp
@@ -1,311 +1,250 @@
-#include <stdio.h> //used for input 
-#include <pthread.h> //used for multithreading
-#include <unistd.h> //used for sysconf, _SC_NPROCESSORS_ONLN, 
-#include <mutex> //used for mutex
-#include <chrono> //used for timing functions
-#include <algorithm> //used for std::copy
-#include <vector>
+#include <stdlib.h> //used for atoi
+#include <stdio.h> //used for printf
+#include <unistd.h> //used for sysconf
+#include <chrono> //used for timekeeping variables
+#include <vector> //used for the vector datatype 
+#include <pthread.h> // used for pthread_create, pthread_join
+#include <algorithm> //used for std::next_permutation
 
-std::mutex mu;
+
+//typedef used to make definition of large variables more readable
+typedef  unsigned long largeNum;
+
 //function prototypes
-void print_arr(int*,int, FILE*); 
-void* thread_ordering_creator(void*);
-int verify_ordering(int*,int);
-int* get_starting_baselist(int*, int, long int);
-unsigned long factorial(int);
-int* get_tuple(int, unsigned long);
-int* get_list(int*, int, int);
-void print_vector(std::vector<int>);
+largeNum factorial(int);
+int* lookupOrdering(int, largeNum);
+void* threadProcessorFunc(void*);
+int verifyOrdering(int*, int);
 
-//Struct to hold the parameters to provide to the function each thread is processing
+
+//definition of the struct used to pass information to each thread
 struct Thread_Param{
-	int id;//the id of the thread
-	int* baselist;// a pointer to the ordering to start calculation from
-	int n;//the size of the group to calculate the permutations for; Z/nZ
-	long int partition_size;//the number of permutations to calculate
-	bool results;
+	int id; // the id of the thread, beginning at 0
+	int n; // the size of the group Z/nZ
+	largeNum partitionSize;// the number of orderings for the thread to process
+	int* firstOrdering;//a pointer to an array corresponding to the first ordering the thread will process
+	//default constructor; not used
 	Thread_Param(){}
-	Thread_Param(int i, int* b, int n_val, long int ps, bool res){
-	 	id = i;
-		n = n_val;
-	 	baselist = new int[n-1];
-	 	for(int i=0;i<n-1;i++){
-	 		baselist[i] = b[i];
-	 	}
-	 	partition_size = ps;
-	 	results = res;
+	//main constructor used; takes individual values and sets the corresponding members of the struct
+	Thread_Param(int id, int n, largeNum partitionSize){
+		//set the member variables passed in
+		this->id = id;
+		this->n = n;
+		this->partitionSize = partitionSize;
+		//calculate the ordering this thread will begin calculation with
+		this->firstOrdering = lookupOrdering(n, id*partitionSize);
 	}
-	Thread_Param(Thread_Param& t){//Copy Constructor for Thread_Param
-	 	id = t.id;
-	 	n = t.n;
-	 	partition_size = t.partition_size;
-	 	baselist = t.baselist;
-	 }
+	//deconstructor; frees firstOrdering
 	~Thread_Param(){
-		delete [] baselist;
-	 }
+		delete [] firstOrdering;
+	}
+
 };
 
+
+/**
+*	The main method of the program. This program calculates the number of constructive orderings on the integers mod n
+*/
 int main(int argc, char const *argv[])
 {
-	if(argc<2){//if usage is incorrect, exit
-		fprintf(stderr, "USAGE: %s n [THREAD_MULT] [SAVE_FLAG]\n", argv[0]);
-		_exit(1);
-	}
-	int n = atoi(argv[1]);//store the first command line argument, the value for n
-	int thread_mult = 1;
-	if(argc>2){//if there's more than one command-line argument, the second arg is the thread multiplier
-		thread_mult = atoi(argv[2]);
-	}
-	int max_threads = thread_mult*sysconf(_SC_NPROCESSORS_ONLN);//get the number of on-line concurrent threads. The number of child processes created is calculated as a multiple of the number of concurrent threads as optionally given by the user on program initialization
-	
-	if(n<(max_threads/thread_mult)){
-		fprintf(stderr, "[ERROR] Command Line argument too small; must be greater than the number of concurrent threads, %d\n", max_threads);
-		_exit(3);
-	}
-	bool save_results = false;
-	//if there is a third argument equal to 1, then save the list of constructive orderings
-	if(argc>3){
-		if(atoi(argv[3])==1){
-			save_results = true;
-		}
-	}
-	if(save_results){
-		printf("Saving results..\n");
-	}
-	std::chrono::time_point<std::chrono::system_clock> start, end;//create the timekeeping variables
-	start = std::chrono::system_clock::now();//initialize the first time variable
-	
-	long total_perms = factorial(n-1);//calculate the total number of permutations to verify. We use n-1 because the first value of a constructive ordering is known to be a fixed value (0), so we do not need to verify orderings that do not begin with 0
-	unsigned long partition_size = (total_perms/max_threads)/2;//calculate the number of permutations each thread should process. We divide by two because each ordering of the form (0,k,...), where k is less than n/2, can be mapped to exactly one ordering of the form (0,l,...), where l is greater than n/2
-	if(save_results){
-		if(argc>4){//if we set this 4th parameter, use the full list, instead of the half list for full results
-			if(atoi(argv[4])==1){
-				partition_size = total_perms/max_threads + 1;
-			}	
-		}
-		
-	}
-	printf("MAX THREADS: %d\n", max_threads);
-	pthread_t threads[max_threads];//create the array of threads
-	int* baselist = NULL;// pointer to point to the first ordering each thread begins with
-	for(int i=0;i<max_threads;i++){//for each thread,
-		//baselist = get_starting_baselist(baselist, n, partition_size); //generate the starting ordering for the thread
-		baselist = get_list(get_tuple(n-1, partition_size*i),n,i);
-		Thread_Param* tp = new Thread_Param(i,baselist, n, partition_size,save_results);//create the Thread_Param object to supply to the function provided to each thread//allocate it dynamically so each thread has its own struct
-		int t_status = pthread_create(&threads[i], NULL, thread_ordering_creator,(void*)tp);//create the thread, having it compute the thread_ordering_creator function
-		if(t_status!=0){//if there is an error creating the thread, exit
-			fprintf(stderr, "[ERROR] Error creating thread %d; status is %d; exiting\n", i, t_status);
-			_exit(2);
-		}
-
-	}
-	long int total_good_perms = 0;//variable to hold the final result
-
-	//loop to process each thread
-	for(int i=0;i<max_threads;i++){
-		void* results_ptr;
-		pthread_join(threads[i], &results_ptr);//join each thread as it finishes
-		mu.lock();
-		total_good_perms+= (long)(results_ptr);//ensure the results variable is locked to be processed single-threadedly, to avoid write collisions
-		mu.unlock();
-	}
-
-	total_good_perms*=2;
-	end = std::chrono::system_clock::now();//get the ending time
-	std::chrono::duration<double> time_taken = end-start;
-	double total_time = time_taken.count();
-	printf("FINISHED - Total good orderings: %ld - Time taken: %f\n", total_good_perms, total_time);
-	printf("%d,%f\n", thread_mult, total_time);	
-	//if we're saving the lists, we don't care about performance, because it will always be faster to not print the orderings. This helps clean up program output. Redirect stderr to a file to capture the results
-	if(save_results){
-		fprintf(stderr, "%d,%f\n", thread_mult, total_time);
-	}
-	return 0;
-
-
-}
-
-
-
-/**
-*	This function confirms or denies if ord is a valid ordering
-*	INPUT:
-*		- ord: a pointer to an int array representing an ordering to verify
-*		- size: the size of the ordering
-*		- tid: the thread id of the thread ord belongs to
-*		- results: a flag used to save results
-*	OUTPUT:
-		- returns 1 if ord is a constructive ordering, and 0 otherwise
-*/
-
-int verify_ordering(int* ord, int size, int tid, bool results){
-	int total = 0;
-	int n = size+1;
-	bool* elements_seen = new bool[n]();//use an array to keep track of values already seen
-	for(int i=0;i<size;i++){
-		total += ord[i];
-		total %= n;
-		//if the element was found previously, or if the sum is 0, discard this ordering. 
-		if(elements_seen[total] || total==0 || (total==n/2 && i!=size-1)){
-			delete [] elements_seen;
-			return 0;
-		}
-		else{//otherwise, set the flag at index total to true to indicate the value has been seen before.
-			elements_seen[total]=true;
-		}
-	}
-	//if we make it here, the ordering is a constructive ordering
-	if(results){
-		mu.lock();
-		print_arr(ord, size, stderr);
-		mu.unlock();
-	}
-	delete [] elements_seen;
-	return 1;
-}
-
-/**
-*	This method calculates the orderings for each thread to begin with. The thread then processes each ordering, deriving each successive ordering
-*	INPUT:
-*		- old_baselist: the ordering the previous thread began with
-*		- n: the size of the group as provided by the user
-*		- partition_size: the number of orderings to calculate per thread
-*	OUTPUT:
-*		Returns the ordering for the next thread to start verifying from
-*/
-int* get_starting_baselist(int* old_baselist, int n, long int partition_size){
-	int v = n-1;
-	long int list_count = 0;
-	int* perm_list = new int[v];// array to return; the ordering for the thread to start with
-	if(old_baselist==NULL){// if this is the first thread, use the natural ordering (0,1,2,...,n-1) for this thread
-		for(int i=0;i<v;i++){
-			perm_list[i] = i+1;
-		}
-		return perm_list;
-	}
-	//otherwise
-	for(int i=0;i<v;i++){//create the first ordering
-		perm_list[i] = old_baselist[i];
-	}
-	do{//iterate through successive orderings
-		list_count++;
-		if(list_count>=partition_size){
-			break;
-		}
-	}while(std::next_permutation(perm_list, perm_list+v));
-	delete old_baselist;
-	return perm_list;
-}
-
-/**
-*	This function prints the first size elements from arr
-*	INPUT:
-*		- arr: the array to print
-*		- size: the number of elements in arr
-*		- f: the file to print to. By default, this is set as stdout to print to the terminal
-*/
-
-void print_arr(int* arr, int size, FILE* f=stdout){
-	for(int i=0;i<size;i++){
-		fprintf(f, "%d",arr[i]);
-		if(i!=size-1){
-			fprintf(f, ",");
-		}
-	}
-	fprintf(f,"\n");
-	return;
-}
-
-/**
-*	This function iterates through the orderings each thread is responsible for checking
-*		INPUT:
-*			- args: a pointer to a Thread_Param struct
-*		OUTPUT:
-*			- the number of constructive orderings in the bounds for this thread 	
-*/
-void* thread_ordering_creator(void* args){
-	// Get the Thread_Param struct out of args
-	Thread_Param* params = (Thread_Param*)(args);	
-	int v = params->n-1;
-	int* iterate_list = new int[v];//list used to iterate through
-	for(int i=0;i<v;i++){
-		iterate_list[i] = params->baselist[i];//deepcopy into iterate_list
-	}
-	long int good_orderings_calculated = 0;
-	long int total_orderings_seen = 0;
-	do{	
-		int is_good_ordering = verify_ordering(iterate_list, v, params->id, params->results);//verify this ordering
-		good_orderings_calculated += is_good_ordering;
-		total_orderings_seen++;
-		if(total_orderings_seen>params->partition_size-1){
-			break;
-		}
-	}while(std::next_permutation(iterate_list, iterate_list+v));//iterate through each permutation
-
-	delete [] iterate_list;
-	delete params;
-	return (void*)good_orderings_calculated;
-}
-
-
-/**
-*	This is a simple recursive implementation of the factorial
-*	INPUT:
-*		- n: the value to calculate the factorial of
-*	OUTPUT:
-*		returns n!
-*/
-unsigned long factorial(int n){
-	if(n<0)
-		return -1;
-	if(n==0 || n==1)
+	//exit and print usage information if the program is run with 0 args
+	if(argc==1){
+		fprintf(stderr, "USAGE: %s n threadMult\n", argv[0]);
 		return 1;
-	else{
-		return n*factorial(n-1);
 	}
+	//get program parameters from the command line as the program is executed
+	int n = atoi(argv[1]);
+	int threadMult = 1;
+	//set threadMult if provided by the user
+	if(argc==3){
+		threadMult = atoi(argv[2]);
+	}
+
+	//calculate the total number of threads to run based on the multiplier provided by the user and the total number of online processors at the time of program execution
+	int maxThreads = threadMult*sysconf(_SC_NPROCESSORS_ONLN);
+
+	printf("n is %d, threadMult is %d, total threads to create is %d\n", n, threadMult, maxThreads);
+	//the total number of orderings to calculate; Since we only care about orderings that start with zero, we only have (n-1)! orderings to check, and since each constructive ordering of the form (0, k, ...), k<n/2 has exactly one corresponding constructive ordering (0,n-k,...), we only need to check the first half of the orderings, hence the division by 2
+	largeNum orderingCount = factorial(n-1)/2;
+
+	//if there arent an evenly divisible number of orderings per thread, we can't continue
+	if(orderingCount%maxThreads!=0){
+		fprintf(stderr,"[ERROR] The number of orderings (%ld) is not evenly divisible across the number of threads (%d)\n", orderingCount, maxThreads);
+		return 2;
+	}	
+	//each thread will process exactly partitionSize orderings
+	largeNum partitionSize = orderingCount/maxThreads;
+
+	//create an array of thread objects
+	pthread_t threads[maxThreads]; 
+
+
+	//create the variables used for timekeeping
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+
+	for(int i=0;i<maxThreads;i++){
+		Thread_Param* tp = new Thread_Param(i, n, partitionSize);
+		int threadStatus = pthread_create(&threads[i], NULL, threadProcessorFunc,(void*)tp);
+		if(threadStatus!=0){// if there is an error creating the thread, exit
+			fprintf(stderr, "[ERROR] Error creating thread %d\n", i);
+			return 3;
+		}
+	}
+
+	largeNum constructiveOrderings = 0;
+
+	for(int i=0;i<maxThreads;i++){
+		void* results = NULL;
+		pthread_join(threads[i], &results);
+		constructiveOrderings+=(largeNum)(results);
+	}
+
+	//multiply the final result by 2, to compensate for the fact that only the first half of orderings are checked.
+	constructiveOrderings*=2;
+
+	end = std::chrono::system_clock::now();
+	std::chrono::duration<double> timeTaken = end-start;
+	printf("FINISHED - Total constructive orderings: %ld - Time taken: %f\n", constructiveOrderings, timeTaken.count());
+
+
+	return 0;
 }
 
 /**
-*	This method generates a tuple representing the coefficients of tuple_num's representation in factorial base. This is used to calculate the tuple_numth lexicographical permutation.
+*	A simple recursive definition of the factorial function
 *	INPUT:
-		- m: the size of the group minus 1, i.e. n-1
-		- tuple_num: the value to convert to factorial base.
+		- n: the value to calculate the factorial of
 	OUTPUT:
-		- an array of size m representing a tuple of coefficients of the factorial representation of tuple_num
+		returns n!
 */
-int* get_tuple(int m, unsigned long tuple_num){
-	int* tuple = new int[m];
-	for(int i=0;i<m;i++){
-		int v = tuple_num/factorial(m-i);
-		tuple[i] = v;
-		tuple_num=tuple_num-factorial(m-i)*v;
+largeNum factorial(int n){
+	if(n>1){
+		return factorial(n-1)*n;
 	}
-	return tuple;
+	else{
+		return 1;
+	}
 }
 
-int* get_list(int* tuple, int n, int id){
-	std::vector<int> ordered_list;
-	for(int i=0;i<n;i++){
-		ordered_list.push_back(i);
-	}
-	int* list = new int[n];
+/*
+*	This method calculates the lexicographical ordering based on a given index for the integers mod n, minus the first element.
+	Example:
+	All permutations of the integers mod 3 are listed as follows, in lexicographical order:
+	(0,1,2)
+	(0,2,1)
+	(1,0,2)
+	(1,2,0)
+	(2,0,1)
+	(2,1,0)
+
+	Indexing these in this order gives the following relation:
+	0 -> (0,1,2)
+	1 -> (0,2,1)
+	2 -> (1,0,2)
+	3 -> (1,2,0)
+	4 -> (2,0,1)
+	5 -> (2,1,0)
+
+	Thus, lookupOrdering(3,4), for example, would return [0,1], which is [2,0,1] without the first element.
+
+	INPUT:
+		- n: the size of the group
+		- orderingIndex: the index of the ordering we are interested in
+	OUTPUT:
+		- the ordering with index orderingIndex, in lexicographical order, minus the first element
+*/
+int* lookupOrdering(int n, largeNum orderingIndex){
+	//tuple is an array which holds the factorial representation of orderingIndex
+	int* tuple = new int[n-1];
 	for(int i=0;i<n-1;i++){
-		list[i] = ordered_list.at(tuple[i]);
-		ordered_list.erase(ordered_list.begin()+tuple[i]);
+		//calculate the ith coefficient of orderingIndex
+		int coefficient = orderingIndex/factorial(n-1-i);
+		tuple[i] = coefficient;
+		//reduce orderingIndex to calculate the next coefficient
+		orderingIndex-=factorial(n-1-i)*coefficient;
 	}
-	list[n-1] = ordered_list.front();
+
+	std::vector<int> orderedList;
+	for(int i=0;i<n;i++){
+		orderedList.push_back(i);
+	}
+	int* ordering = new int[n];
+	for(int i=0;i<n-1;i++){
+		ordering[i] = orderedList.at(tuple[i]);
+		orderedList.erase(orderedList.begin()+tuple[i]);
+	}
+	ordering[n-1] = orderedList.front();
 
 	int* nl = new int[n-1];
 	for(int i=0;i<n-1;i++){
-		nl[i] = list[i+1];
+		nl[i] = ordering[i+1];
 	}
+
+	delete [] ordering;
+	delete [] tuple;
 	return nl;
-	
+
+
 }
 
-void print_vector(std::vector<int> v){
-	for(int i=0;i<v.size();i++){
-		printf("v[%d]: %d\n", i, v.at(i));
+
+/**
+*	This method is used by each thread to begin calculation.
+*	INPUT:
+*		- args: a pointer which is cast into a Thread_Param pointer, used to access parameters intended for the method.
+	OUTPUT:
+		- the number of constructive orderings in the range for the thread, cast into a void pointer
+*/
+void* threadProcessorFunc(void* args){
+	//cast args into a Thread_Param struct
+	Thread_Param* params = (Thread_Param*)(args);
+
+	//extract the members of the params struct
+	int n = params->n;
+	largeNum partitionSize = params->partitionSize;
+	int* currentOrdering = params->firstOrdering;
+	//constructiveOrderings holds the count of how many constructive orderings are in the range of the thread
+	largeNum constructiveOrderings = 0;
+
+	do{//iterate through each permutation in range and verify each of them.
+		constructiveOrderings+=verifyOrdering(currentOrdering, n);
+		partitionSize--;
+		if(partitionSize==0){
+			break;
+		}
+	}while(std::next_permutation(currentOrdering, currentOrdering+(n-1)));
+
+	//clean up allocated memory and return constructiveOrderings
+	delete params;
+	return (void*)(constructiveOrderings);
+
+
+}
+
+/**
+	This method determines if a given ordering is a constructive ordering.
+	INPUT:
+		- ordering: an integer array of size n-1 representing a potential constructive ordering.
+		- n: the size of the group
+	OUTPUT:
+		returns 1 if ordering is a constructive ordering, and 0 otherwise.
+*/
+int verifyOrdering(int* ordering, int n){
+	int total = 0;
+	bool* elementsSeen = new bool[n]();
+	for(int i=0;i<n-1;i++){
+		total += ordering[i];
+		total %= n; 
+		if(total==0 || elementsSeen[total]==true || (total==n/2 && i!=n-2)){
+			delete [] elementsSeen; 
+			return 0;
+		}
+		else{
+			elementsSeen[total] = true;
+		}
 	}
+
+	delete [] elementsSeen;
+	return 1;
 }
